297a1eb3b7018513bafb2a5a303404a9546e4c99
Fabio Berner <fabberne@student.ethz.ch>
2025-03-03 23:24:24 +0100

    initial commit

---
+++ material.py
@@ -0,0 +1,78 @@
+class Material:
+
+    def __init__(self, gamma, E, f_druck, f_zug):
+
+        self.gamma = gamma
+        self.E     = E
+        self.n     = 0.5
+
+        self.f_druck = f_druck
+        self.f_zug   = f_zug
+
+class Concrete_C30_37(Material):
+
+    def __init__(self):
+
+        gamma   = 25 * 10**(-6)  # N/mm3
+        E       = 32000          # N/mm2
+        f_druck = 20             # N/mm2
+        f_zug   = 0.27           # N/mm2
+
+        super().__init__(gamma, E, f_druck, f_zug)
+
+        self.color = (0, 0, 0, 0.5)
+
+
+class Steel_S235(Material):
+
+    def __init__(self):
+
+        gamma   = 78.5 * 10**(-6) # N/mm3
+        E       = 210000          # N/mm2
+        f_druck = 235             # N/mm2
+        f_zug   = 235             # N/mm2
+
+        super().__init__(gamma, E, f_druck, f_zug)
+
+        self.color = (0, 0, 1, 0.5)
+
+
+class Steel_S355(Material):
+
+    def __init__(self):
+
+        gamma   = 78.5 * 10**(-6) # N/mm3
+        E       = 210000          # N/mm2
+        f_druck = 355             # N/mm2
+        f_zug   = 355             # N/mm2
+
+        super().__init__(gamma, E, f_druck, f_zug)
+
+        self.color = (0, 0, 1, 0.5)
+
+
+class Rebar_B500B(Material):
+
+    def __init__(self):
+
+        gamma   = 78.5 * 10**(-6) # N/mm3
+        E       = 205000          # N/mm2
+        f_druck = 435             # N/mm2
+        f_zug   = 435             # N/mm2
+
+        super().__init__(gamma, E, f_druck, f_zug)
+
+        self.color = (0, 0.2, 1, 0.5)
+
+class Unknown(Material):
+
+    def __init__(self):
+
+        gamma   = 1
+        E       = 1
+        f_druck = 1
+        f_zug   = 1 
+
+        super().__init__(gamma, E, f_druck, f_zug)
+
+        self.color = (1, 0, 0, 0.5)
